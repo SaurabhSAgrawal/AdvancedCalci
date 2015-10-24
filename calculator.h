96c531db24175ce321a296841888ec1e6367524c
SaurabhSAgrawal <saurabhagrawal1483@gmail.com>
2015-10-24 17:10:50 +0530

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -8,15 +8,14 @@ GtkWidget* text_field;
 
 gchar* return_button_label(gint index);
 
-void callback( GtkWidget *widget,gpointer data );
-
-int return_symbol(char* text_field_buffer, int* index, char* symbol);
-
-void make_string(char* text_field_buffer,char* dest1,char* dest2,int index);
+void callback( GtkWidget *widget,gpointer data);
 
 void clearbuffer(char* text_field_buffer);
 
 int calculation(char* text_field_buffer);
 
-void apply_currect_operation(char* output, char symbol, double num1, double num2);
-
+int precedance(char c);
+void operation(char c);
+int infixeval(char *a);
+void itoa(int n,char s[]);
+void reverse(char s[]);
