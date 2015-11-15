8dd7a97da8815beae8b4ac011493ad3cf67cfee8
SaurabhSAgrawal <saurabhagrawal1483@gmail.com>
2015-11-15 16:50:54 +0530

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -1,3 +1,22 @@
+
+/*****************************************************************************
+ * Copyright (C) Saurabh Agrawal saurabhagrawal1483@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include <string.h>
 #include<stdio.h>
 #include <ctype.h>
@@ -14,8 +33,16 @@ void clearbuffer(char* text_field_buffer);
 
 int calculation(char* text_field_buffer);
 
-int precedance(char c);
+int precedence(char c);
+
 void operation(char c);
+
 int infixeval(char *a);
+
 void itoa(int n,char s[]);
+
 void reverse(char s[]);
+
+long long int fact(long long int n);
+
+char* fact1(int n);
